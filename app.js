853b131f07d86f021a3e9efedcb1a0d675a6aca2
Andy Carello <andreacarello@hotmail.com>
2022-08-29 20:37:14 -0300

    Agrego Heroku

--- app.js
+++ app.js
@@ -2,13 +2,16 @@ const express = require ("express");
 const app = express ();
 const path = require("path");
 app.use(express.static("public"));
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("Servidor corriendo")
 });
 app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
 });
 app.get("/register", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/register.html"))
+});
+app.post("/register", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
 
