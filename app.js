6e9a6b3047c61bfbbbe351baa9a0b42aea3f0994
Andy Carello <andreacarello@hotmail.com>
2022-08-30 00:22:37 -0300

    Creo que terminado

--- app.js
+++ app.js
@@ -3,12 +3,17 @@ const app = express ();
 const path = require("path");
 app.use(express.static("public"));
 
-app.get("/", (req,res)=>{
+/* app.get("/", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/test.html"))
 });
 app.get("/home", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/home.html"))
+}); */
+
+app.get("/", (req,res)=>{
+    res.sendFile(path.join(__dirname, "/views/home.html"))
 });
+
 app.get("/register", (req,res)=>{
     res.sendFile(path.join(__dirname, "/views/register.html"))
 });
